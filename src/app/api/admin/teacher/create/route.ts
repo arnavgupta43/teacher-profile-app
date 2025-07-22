01592c9e26a2205c8c0f985c6ddf119506993100
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-07-22 09:53:10 +0530

    fix:using formdata

--- src/app/api/admin/teacher/create/route.ts
+++ src/app/api/admin/teacher/create/route.ts
@@ -6,20 +6,20 @@ import Teachers from "@/models/teacherModel";
 export async function POST(req: NextRequest) {
   try {
     await connect();
-    const body = await req.json();
-    const {
-      username,
-      name,
-      email,
-      password,
-      age,
-      mobileNo,
-      address,
-      photo,
-      previousExperience,
-      researchInterests,
-      publications,
-    } = body;
+    const formdata = await req.formData();
+    const username = formdata.get("username") as string;
+    const name = formdata.get("name") as string;
+    const email = formdata.get("email") as string;
+    const password = formdata.get("password") as string;
+    const ageStr = formdata.get("age") as string;
+    const mobileNo = formdata.get("mobileNo") as string;
+    const address = formdata.get("address") as string;
+    const photo = formdata.get("photo") as string;
+    const previousExperience = formdata.get("previousExperience") as string;
+    const researchInterests = formdata.getAll("researchInterests") as string[];
+    const publications = formdata.getAll("publications") as string[];
+    const age = Number(ageStr);
+
     const profile = {
       photo,
       age,
